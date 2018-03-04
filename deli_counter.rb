315516077aed7af03751939c8ae5525475d07a15
Victoria Meng <nights1ststar@protonmail.com>
2018-03-04 03:07:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,11 @@ def line(line)
   if line.empty?
     puts "The line is currently empty."
   else
-    
+
   end
 end
 
-def take_a_number
+def take_a_number(line, name)
+  line << name
+  puts "Welcome, #{name}. You are #{line.index(name) + 1}."
 end
-
-
